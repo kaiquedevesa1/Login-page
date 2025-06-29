685838bdf198965184b1e6d42b8739bdd0ce5224
kaiquedevesa1 <kaikedaeves@hotmail.com>
2025-06-29 20:13:47 -0300

    add input email

--- src/app/components/primary-input/primary-input.component.ts
+++ src/app/components/primary-input/primary-input.component.ts
@@ -5,6 +5,7 @@ type InputTypes = 'text' | 'email' | 'password';
 
 @Component({
   selector: 'app-primary-input',
+  standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './primary-input.component.html',
   styleUrl: './primary-input.component.scss',
@@ -13,4 +14,5 @@ export class PrimaryInputComponent {
   @Input() type: InputTypes = 'text';
   @Input() formName: string = '';
   @Input() placeholder: string = '';
+  @Input() label: string = '';
 }
